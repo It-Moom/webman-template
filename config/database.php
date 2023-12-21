2fc1df7812081f291271abc3c8a2dac328520b3b
GabbyMrH <gabbymrh@gmail.com>
2023-12-21 18:11:11 +0800

    feat:增加orm组件

--- config/database.php
+++ config/database.php
@@ -12,4 +12,51 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
-return [];
+return [
+    // 默认数据库
+    'default'     => 'mysql',
+    // 各种数据库配置
+    'connections' => [
+        'mysql'  => [
+            'driver'      => 'mysql',
+            'host'        => getenv('DB_HOST', '127.0.0.1'),
+            'port'        => getenv('DB_PORT', 3306),
+            'database'    => getenv('DB_DATABASE', 'webman'),
+            'username'    => getenv('DB_USERNAME', 'webman'),
+            'password'    => getenv('DB_PASSWORD', ''),
+            'unix_socket' => '',
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_unicode_ci',
+            'prefix'      => getenv('DB_PREFIX', ''),
+            'strict'      => true,
+            'engine'      => null,
+        ],
+        'sqlite' => [
+            'driver'   => 'sqlite',
+            'database' => '',
+            'prefix'   => '',
+        ],
+        'pgsql'  => [
+            'driver'   => 'pgsql',
+            'host'     => '127.0.0.1',
+            'port'     => 5432,
+            'database' => 'webman',
+            'username' => 'webman',
+            'password' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
+        ],
+        'sqlsrv' => [
+            'driver'   => 'sqlsrv',
+            'host'     => 'localhost',
+            'port'     => 1433,
+            'database' => 'webman',
+            'username' => 'webman',
+            'password' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+        ],
+    ],
+];
